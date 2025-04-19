227a325bb98374f31da1ac7a043433378c2e3259
kiripupsik5 <kirillpup227@gmail.com>
2025-04-19 18:44:39 +0300

    Frontend launch

---
+++ doers/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Test",
+  description: "Test metadata",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className='antialiased'>
+        {children}
+      </body>
+    </html>
+  );
+}
