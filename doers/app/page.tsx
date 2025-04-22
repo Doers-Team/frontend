f535da26209463f9615f46938559833b1d570677
kiripupsik5 <kirillpup227@gmail.com>
2025-04-22 13:15:29 +0300

    Add default styles

--- doers/app/page.tsx
+++ doers/app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="flex flex-col justify-center mt-50">
+      <div className="flex justify-center mt-5">
+        <button className="h-15 w-30 m-5 bg-link text-bg text-xl rounded-xl">Button</button>
+        <button className="h-15 w-30 m-5 bg-info text-bg text-xl rounded-xl">Button</button>
+        <button className="h-15 w-30 m-5 bg-success text-bg text-xl rounded-xl">Button</button>
+        <button className="h-15 w-30 m-5 bg-warning text-bg text-xl rounded-xl">Button</button>
+        <button className="h-15 w-30 m-5 bg-danger text-bg text-xl rounded-xl">Button</button>
+      </div>
+    </div>
   );
 }
